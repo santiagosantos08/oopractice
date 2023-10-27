06af82027336ca3322500cb78712634a40dbfda0
santiago <santiagosantos08@gmail.com>
2023-10-27 08:44:13 -0300

    added 2023p

--- src/recu2021/filtros/FiltroTrue.java
+++ src/recu2021/filtros/FiltroTrue.java
@@ -1,9 +1,11 @@
 package recu2021.filtros;
 
+import recu2021.modelo.Cliente;
+
 public class FiltroTrue implements Filtro {
     public FiltroTrue(){
 
     }
     @Override
-    public boolean cumple(){return true;}
+    public boolean cumple(Cliente c){return true;}
 }
