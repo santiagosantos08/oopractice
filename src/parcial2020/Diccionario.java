7ef3d3096444773e7faaa26eb5eaf606bc839809
santiago <santiagosantos08@gmail.com>
2023-11-02 20:40:15 -0300

    ..

--- src/parcial2020/Diccionario.java
+++ src/parcial2020/Diccionario.java
@@ -3,6 +3,7 @@ package parcial2020;
 import java.util.Map;
 
 public class Diccionario {
+    /*
     private static Map<String, String> inglesIngles;
     private static Map<String, String> inglesAleman;
     private static Map<String, String> inglesFrances;
@@ -12,37 +13,19 @@ public class Diccionario {
     private static Map<String, String> francesIngles;
     private static Map<String, String> espanolIngles;
     private static Map<String, String> italianoIngles;
-
+     */
     private static Map<String,Map<String,String>> deInglesA;
     private static Map<String,Map<String,String>> aInglesDe;
 
-    public static void setMapas(Map<String,String> ninglesIngles,Map<String,String> ninglesAleman,
-                           Map<String,String> ninglesFrances,Map<String,String> ninglesEspanol,
-                           Map<String,String> ninglesItaliano,Map<String,String> nalemanIngles,
-                           Map<String,String> nfrancesIngles,Map<String,String> nespanolIngles,
-                           Map<String,String> nitalianoIngles) {
-        //posiblemente hacerles new para que no sean EL MISMO..
-        inglesIngles = ninglesIngles;
-        inglesAleman = ninglesAleman;
-        inglesFrances = ninglesFrances;
-        inglesEspanol = ninglesEspanol;
-        inglesItaliano = ninglesItaliano;
-        alemanIngles = nalemanIngles;
-        francesIngles = nfrancesIngles;
-        espanolIngles = nespanolIngles;
-        italianoIngles = nitalianoIngles;
+    public static void setMapas(Map<String,Map<String,String>> engTo,Map<String,Map<String,String>> fromEng) {
         deInglesA.clear();
         aInglesDe.clear();
-        deInglesA.put("Ingles", inglesIngles);
-        deInglesA.put("Aleman", inglesAleman);
-        deInglesA.put("Frances", inglesFrances);
-        deInglesA.put("Espanol", inglesEspanol);
-        deInglesA.put("Italiano",inglesItaliano);
-        aInglesDe.put("Ingles", inglesIngles);
-        aInglesDe.put("Aleman", alemanIngles);
-        aInglesDe.put("Frances",francesIngles);
-        aInglesDe.put("Espanol", espanolIngles);
-        aInglesDe.put("Italiano", italianoIngles);
+        deInglesA = engTo;
+        aInglesDe = fromEng;
+        /*
+         * deInglesA :: Map<String::lengDestino,Map<String::palabraIngles,String::palabraLengDestino>>
+         * aInglesDe :: Map<String::lengOrigen,Map<String::palabralengOrigen,String::palabraIngles>>
+         */
     }
     public static String traducir(String origen, String destino, String palabra){
         String res = null;
